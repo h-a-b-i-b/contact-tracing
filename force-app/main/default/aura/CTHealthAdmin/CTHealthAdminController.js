d69f3ed8ddd8d6045d7eec3ae35b5744efd99443
MD HABIBULLA <67001264+h-a-b-i-b@users.noreply.github.com>
2026-05-21 07:46:51 +0530

    CT Challenge 17 - Make Interactive With Javascript | Part 1

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
+    handleTabClick : function(component, event, helper) {
+        // Selected tab id
+        var selectedTabId = component.get("v.selectedTabId");
 
+        // Create a lookup map for your labels
+        var tabLabels = {
+            "person_view": "Person View",
+            "location_view": "Location View"
+        };
+
+        component.set("v.selectedTabLabel", tabLabels[selectedTabId]);
     }
 })
