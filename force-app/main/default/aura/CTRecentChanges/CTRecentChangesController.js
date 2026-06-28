2b8144a7e6cf894eec0dd9242a438f5a2ee5af1f
MD HABIBULLA <67001264+h-a-b-i-b@users.noreply.github.com>
2026-06-28 07:16:47 +0530

    CT Challenge 24

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -1,12 +1,17 @@
 ({
     init: function (cmp, event, helper) {
 
+        var actions = [
+            { label: 'View/Update', name: 'show_details', type: 'button' }
+        ]
+
         const personColumns = [
             { label: "Person Name", fieldName: "Name", type: "text" },
             { label: "Phone", fieldName: "Mobile__c", type: "phone" },
             { label: "Token", fieldName: "Token__c", type: "text" },
             { label: "Health Status", fieldName: "Health_Status__c", type: "text" },
-            { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+            { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+            { type: "action", typeAttributes: { rowActions: actions } }
         ];
 
         const locationColumns = [
@@ -15,7 +20,8 @@
             { label: "Pincode", fieldName: "Pincode__c", type: "text" },
             { label: "Address", fieldName: "Address__c", type: "text" },
             { label: "Red Score", fieldName: "Red_Score__c", type: "number" },
-            { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+            { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+            { type: "action", typeAttributes: { rowActions: actions } }
         ];
 
         const scope = cmp.get("v.scope");
@@ -32,5 +38,26 @@
         var searchTerm = cmp.find('enter-search').get('v.value');
 
         helper.filterDataByTerm(cmp, searchTerm);
+    },
+
+    handleRowAction: function (cmp, evt, helper) {
+        console.log('Handle Row Action fired...');
+
+        const action = evt.getParam('action');
+        const row = evt.getParam('row');
+        const scope = cmp.get("v.scope");
+        
+        switch (action.name) {
+            case 'show_details': {
+                const appEvent = scope === "person" ? $A.get("e.c:CTPersonSelect") : $A.get("e.c:CTLocationSelect");
+        
+                appEvent.setParams({
+                    "recordId": row.Id,
+                    "status": scope === "person" ? row.Health_Status__c : row.Status__c
+                });
+                appEvent.fire();
+                break;
+            }
+        }
     }
 })
